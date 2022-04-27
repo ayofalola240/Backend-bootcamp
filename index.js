055a7b0c577c423b8ac6e71498c6dc91658ac9ae
Falola Ibrahim <ayofalola240@gmail.com>
2022-04-27 12:28:00 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("HELLO WORLD");
+const fs = require('fs')
+
+////Syncronous way////
+/*const textIn = fs.readFileSync('./txt/input.txt','utf-8')
+console.log(textIn)
+
+const textOut = `This is a new input message: ${textIn}.\nCreated on ${Date.now()}`
+fs.writeFileSync('./txt/output.txt',textOut)
+*/
+
